66d5d28a8b45489eb49e925ecad74bec42fc30fc
ghousia maqsoog <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-04 23:44:01 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# final_45_assignment" 
---
+++ hello_ governor/hello_governor.js
@@ -0,0 +1,5 @@
+#! /usr/bin/evn node
+console.log("Hello Governor Kamran Tessori!!1");
+console.log("Hello Sir Ziaudine!");
+console.log("Hello All of My Teachers!!!");
+export {};
