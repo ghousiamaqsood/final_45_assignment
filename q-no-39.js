e7711d57fbbffc8229c8728cd12a28881fd2c230
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 14:06:02 +0500

    Add files via upload

---
+++ q-no-39.js
@@ -0,0 +1,9 @@
+"use strict";
+// Creating a function with parameters which return a value in string
+function City_Country(city, Country) {
+    return `${city}, ${Country}`;
+}
+// Calling a function and print the returned value
+console.log(City_Country("Karachi", "Pakistan"));
+console.log(City_Country("Tokyo", "Japan"));
+console.log(City_Country("Berlin", "Germany"));
---
+++ q-no-39.ts
@@ -0,0 +1,15 @@
+// Creating a function with parameters which return a value in string
+
+function City_Country(city: string,Country:string) : string {
+return `${city}, ${Country}`;
+}
+
+// Calling a function and print the returned value
+console.log (City_Country("Karachi", "Pakistan"));
+
+console.log(City_Country("Tokyo", "Japan"));
+
+console.log(City_Country("Berlin","Germany"));
+
+
+
