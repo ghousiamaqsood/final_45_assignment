e142ca9c1b7d2555a904d24ad0a022f8f8b374a5
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 16:58:13 +0500

    Add files via upload

---
+++ q_no_24.js
@@ -0,0 +1,19 @@
+"use strict";
+// test for equqlity and inequality with string.
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("Enquality test:", "Ghousia Maqsood" === "Ghousia Maqsood");
+console.log("Enquality test:", "Ghousia Maqsood" === "Fouzia Maqsood");
+// test lower case function
+console.log("lower case test:", "Ghousia Maqsood".toLowerCase() === "ghousia maqsood");
+console.log("lower case test:", "ghousia maqsood".toLowerCase() === "Ghousia Maqsood");
+//Numerical test equality and inequqlity greater than and less than.
+//equality test
+console.log("Equality test:", 1000 === 1000);
+//inequality test.
+console.log("Enquality test:", 10 !== 9);
+//greater than  and equality test.
+console.log("greater than test:", 1000 > 100);
+console.log("greater than and equal to test:", 10 >= 10);
+// less than and equality test.
+console.log("greater than and equal to test:", 10 >= 10);
+console.log("less than test:", 1000 >= 100);
---
+++ q_no_24.ts
@@ -0,0 +1,29 @@
+// test for equqlity and inequality with string.
+
+console.log("Enquality test:",("Ghousia Maqsood"as string)==="Ghousia Maqsood");
+
+console.log("Enquality test:",("Ghousia Maqsood"as string)==="Fouzia Maqsood");
+
+// test lower case function
+
+console.log("lower case test:","Ghousia Maqsood".toLowerCase()==="ghousia maqsood");
+console.log("lower case test:","ghousia maqsood".toLowerCase()==="Ghousia Maqsood");
+
+//Numerical test equality and inequqlity greater than and less than.
+//equality test
+
+console.log("Equality test:",1000===1000)
+
+//inequality test.
+
+console.log("Enquality test:",(10 as number)!==9);
+
+//greater than  and equality test.
+
+console.log("greater than test:",1000>100);
+console.log("greater than and equal to test:",10>=10);
+
+// less than and equality test.
+
+console.log("greater than and equal to test:",10>=10);
+console.log("less than test:",1000>=100);
