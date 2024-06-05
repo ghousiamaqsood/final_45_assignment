a7f7d80d718f04ee4c9b379a74512c786b9f630e
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 22:05:57 +0500

    Add files via upload

--- q_no_24.js
+++ q_no_24.js
@@ -2,10 +2,10 @@
 // test for equqlity and inequality with string.
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Enquality test:", "Ghousia Maqsood" === "Ghousia Maqsood");
-console.log("Enquality test:", "Ghousia Maqsood" === "Fouzia Maqsood");
+console.log("Enquality test:", "Ghousia Maqsood" !== "Fouzia Maqsood");
 // test lower case function
 console.log("lower case test:", "Ghousia Maqsood".toLowerCase() === "ghousia maqsood");
-console.log("lower case test:", "ghousia maqsood".toLowerCase() === "Ghousia Maqsood");
+console.log("lower case test:", "ghousia maqsood".toLowerCase() !== "Ghousia Maqsood");
 //Numerical test equality and inequqlity greater than and less than.
 //equality test
 console.log("Equality test:", 1000 === 1000);
@@ -16,4 +16,13 @@ console.log("greater than test:", 1000 > 100);
 console.log("greater than and equal to test:", 10 >= 10);
 // less than and equality test.
 console.log("greater than and equal to test:", 10 >= 10);
-console.log("less than test:", 1000 >= 100);
+console.log("less than test:", 100 >= 10);
+// Test using "and" and "or"operaator
+console.log("And operator test:", true && true);
+console.log("Or operator test:", true || false);
+//test wether in array
+const car = [1, 2, 3, 4];
+console.log("test number", car.includes(1));
+//test weather not in array 
+const car1 = [1, 2, 3, 4];
+console.log("test number", !car.includes(5));
--- q_no_24.ts
+++ q_no_24.ts
@@ -2,12 +2,12 @@
 
 console.log("Enquality test:",("Ghousia Maqsood"as string)==="Ghousia Maqsood");
 
-console.log("Enquality test:",("Ghousia Maqsood"as string)==="Fouzia Maqsood");
+console.log("Enquality test:",("Ghousia Maqsood"as string)!=="Fouzia Maqsood");
 
 // test lower case function
 
 console.log("lower case test:","Ghousia Maqsood".toLowerCase()==="ghousia maqsood");
-console.log("lower case test:","ghousia maqsood".toLowerCase()==="Ghousia Maqsood");
+console.log("lower case test:","ghousia maqsood".toLowerCase()!=="Ghousia Maqsood");
 
 //Numerical test equality and inequqlity greater than and less than.
 //equality test
@@ -26,4 +26,20 @@ console.log("greater than and equal to test:",10>=10);
 // less than and equality test.
 
 console.log("greater than and equal to test:",10>=10);
-console.log("less than test:",1000>=100);
+console.log("less than test:",100>=10);
+
+// Test using "and" and "or"operaator
+
+console.log("And operator test:",true && true);
+console.log("Or operator test:",true || false);
+
+//test wether in array
+
+const car = [1,2,3,4];
+console.log("test number",car.includes(1));
+
+//test weather not in array 
+
+const car1 = [1,2,3,4];
+console.log("test number",!car.includes(5));
+
