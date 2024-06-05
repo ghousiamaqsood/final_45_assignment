2fd5ff7a460021dd2a5d78f456d28d2566a1feff
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 15:16:34 +0500

    Add files via upload

---
+++ q_no_22.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let errorArray = ["a", "b", "c", "d",];
+//producing Error! by accessing invalid index of array
+//console.log(errorArray[5]]);
+console.log(errorArray[3]);
---
+++ q_no_22.ts
@@ -0,0 +1,6 @@
+let errorArray:string [] = ["a","b","c","d",]
+
+//producing Error! by accessing invalid index of array
+//console.log(errorArray[5]]);
+console.log(errorArray[3]);
+// Error Rmove,
