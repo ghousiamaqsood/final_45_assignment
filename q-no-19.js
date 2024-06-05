a1c5e315d8b6cb53ddae70dd98321b4cbb8ef640
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:43:19 +0500

    Add files via upload

---
+++ q-no-19.js
@@ -0,0 +1,5 @@
+"use strict";
+let guestList = ["Um E Roman", "Anabia", "Qeswa", "Abu bakae",];
+// guestList.forEach(oneGuest =>console.log(`Salam ${oneGuest},you are invited to dinner! We would be honored to have your company"));
+let lengthGuests = guestList.length;
+console.log(`Dear we have inviting total ${lengthGuests} guests,`);
---
+++ q-no-19.ts
@@ -0,0 +1,8 @@
+let guestList: string[] = ["Um E Roman","Anabia","Qeswa","Abu bakae",];
+// guestList.forEach(oneGuest =>console.log(`Salam ${oneGuest},you are invited to dinner! We would be honored to have your company"));
+
+let lengthGuests: number = guestList.length
+console.log(`Dear we have inviting total ${lengthGuests} guests,`)
+
+
+
