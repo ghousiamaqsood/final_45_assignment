9830931e847f0fa24aa7a7f109cde3f95bb35a0c
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 14:02:01 +0500

    Add files via upload

---
+++ q-no-35.js
@@ -0,0 +1,11 @@
+#!/usr/bin/evn node
+"use strict";
+// Creating a Array
+Object.defineProperty(exports, "__esModule", { value: true });
+let petAnimals = ["Cat", "Dog", "Rabbit"];
+// Using for-loop
+for (let onepet of petAnimals) {
+    console.log(`${onepet} would you make a great pet`);
+    // print a message outside of for-loop
+    console.log("Any of these animals would make a agreat pet!S");
+}
---
+++ q-no-35.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/evn node
+// Creating a Array
+
+let petAnimals = [ "Cat", "Dog","Rabbit"]
+
+// Using for-loop
+
+for (let onepet of petAnimals)
+    {
+        console.log(`${onepet} would you make a great pet`)
+       // print a message outside of for-loop
+       console.log("Any of these animals would make a agreat pet!S")
+    }
