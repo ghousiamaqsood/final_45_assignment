6b38c45de6828d28c7c4f3ec272dadca7361b2de
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 15:58:10 +0500

    Add files via upload

---
+++ q_no_23.ts
@@ -0,0 +1,51 @@
+let five =5;
+let ten =10;
+// five test result with true!
+
+//Test 1
+
+console.log("Test 1: five is equal 5");
+console.log(five == 5);
+// Test 2
+console.log("Test 2: ten is equal 10");
+console.log(ten == 10);
+
+// Test 3
+console.log("Test 3: five is not equal to 10");
+console.log(five !==10);
+
+// Test 4
+console.log("Test 4: ten is greatr to 5");
+console.log(ten > 5);
+
+//Test 5
+
+console.log ("Test 5: 5 is less than 10");
+
+console.log(5 <10);
+
+// next five test result with fals!
+
+// test 6
+
+console.log("Test 6: five is  equal 10");
+console.log(ten > 10);
+
+
+// Test 7
+console.log("Test 7: five is equal to 10");
+console.log(five==10);
+
+//Test 8
+console.log("Test 8: five greater to 10");
+console.log(five > 10);
+
+// Test 9
+console.log("Test 9: ten is lees than to 5");
+console.log(ten < 5);
+
+//Test 10
+
+console.log ("Test 10: 10 is less than 10");
+
+console.log(10 < 10);
