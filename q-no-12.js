13b6215cba7600b25567616b94e155401c1d122b
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:33:31 +0500

    Add files via upload

---
+++ q-no-12.js
@@ -0,0 +1,5 @@
+"use strict";
+let names2 = ["kamil", "maqsood", "saira", "kareem",];
+names2.forEach(name => {
+    console.log(`Dear, ${name} Have a Great Day!`);
+});
---
+++ q-no-12.ts
@@ -0,0 +1,7 @@
+let names2: string[] = ["kamil","maqsood","saira","kareem",];
+names2.forEach(name => {
+console.log(`Dear, ${name} Have a Great Day!`)
+})
+
+
+
