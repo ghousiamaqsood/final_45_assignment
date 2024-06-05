b8ba771a59988dc3289ba2ce128f953a7a72b22d
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:19:29 +0500

    Add files via upload

---
+++ q-no-07.js
@@ -0,0 +1,9 @@
+"use strict";
+let addition = 6 + 2;
+console.log(addition);
+let subtrction = 10 - 2;
+console.log(subtrction);
+let multiple = 2 * 4;
+console.log(multiple);
+let divsion = 16 / 2;
+console.log(divsion);
---
+++ q-no-07.ts
@@ -0,0 +1,13 @@
+let addition :number = 6+2
+console.log(addition);
+
+let subtrction : number = 10-2
+console.log(subtrction);
+
+let multiple :number = 2*4
+console.log(multiple);
+
+let divsion :number = 16/2
+console.log(divsion);
+
+ 
