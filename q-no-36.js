249bec4e94b8ee93c7012cf1edd7cb12dbea8ff2
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 14:02:59 +0500

    Add files via upload

---
+++ q-no-36.js
@@ -0,0 +1,10 @@
+"use strict";
+function make_Shirt(size, printMessage) {
+    console.log(`you slected ${size} size shirt with ${printMessage} prnits on shirt`);
+}
+make_Shirt("Large", "Super Power");
+make_Shirt("Medium", "Super Star");
+make_Shirt("Smll", "Super Strength");
+make_Shirt("Large size 65", "Super Power");
+make_Shirt("Medium size 38", "Super Star");
+make_Shirt("Smll size 30", "Super Strength");
