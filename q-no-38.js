0c778437ee0cc8e2f4356759135b79d8cf5ec455
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 14:05:29 +0500

    Add files via upload

---
+++ q-no-38.js
@@ -0,0 +1,9 @@
+"use strict";
+// Describe a Function
+function describe_City(city, Country = "Pakistan") {
+    console.log(`${city} is in ${Country}`);
+}
+// Calling the function
+describe_City("Karachi");
+describe_City("Lahore");
+describe_City("Berlin", "Germany");
---
+++ q-no-38.ts
@@ -0,0 +1,16 @@
+// Describe a Function
+function describe_City (city: string, Country: string = "Pakistan"){
+console.log(`${city} is in ${Country}`);
+
+
+
+}
+
+// Calling the function
+
+
+describe_City("Karachi");
+
+describe_City("Lahore");
+
+describe_City("Berlin", "Germany");
