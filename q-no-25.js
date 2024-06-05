b608d19c9161899d885d0d5f217b496a35cda86b
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 22:54:35 +0500

    Add files via upload

---
+++ q-no-25.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_Color = "green";
+if (alien_Color === "green") {
+    console.log("\nthe playerjust earned 5 points");
+} //pass
+alien_Color = "yellow";
+if (alien_Color === "green") {
+    console.log("\nthe playerjust earned 5 points");
+} //fail
---
+++ q-no-25.ts
@@ -0,0 +1,13 @@
+let alien_Color = "green";
+if (alien_Color ==="green"){
+
+
+console.log("\nthe playerjust earned 5 points");
+} //pass
+
+alien_Color = "yellow";
+if (alien_Color ==="green"){
+
+
+    console.log("the playerjust earned 5 points");
+} //fail
