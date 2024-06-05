f91d80f6dcebbf9fcd98d7fefa902c66ce8d5445
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:47:46 +0500

    Add files via upload

---
+++ q-no-21.js
@@ -0,0 +1,7 @@
+"use strict";
+let itCourse = {
+    CourseNme: "Tjypescript and JavaScript",
+    Location: "Governor House Sindh",
+    OnSiteStudents: 50000
+};
+console.log(itCourse);
---
+++ q-no-21.ts
@@ -0,0 +1,14 @@
+interface itCourse{
+    CourseName: string;
+    Location: string;
+    OnSiteStudents:number;
+
+}
+let itCourse = {
+    CourseNme: "Tjypescript and JavaScript", 
+    Location: "Governor House Sindh",
+    OnSiteStudents: 50000
+ 
+};
+
+console.log(itCourse);
