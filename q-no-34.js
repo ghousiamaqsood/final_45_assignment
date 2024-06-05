d1e3e0d782124c3861ff42625683c1dc19cb9405
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 14:00:53 +0500

    Add files via upload

---
+++ q-no-34.js
@@ -0,0 +1,11 @@
+"use strict";
+// Creating a Array
+let pizza = ["Chicken", "Tikka", "Cheese", "Fajita",];
+// Using for loop
+for (let onepizza of pizza) {
+    console.log(`i like ${onepizza} pizza`);
+}
+// print a message outside for-loop
+console.log("pizza is Love");
+console.log("i crazy for pizza");
+console.log("i am pizza lover");
