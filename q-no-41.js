277fc66522cc12fe99a9cc61293e087ae6e343d9
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 14:07:31 +0500

    Add files via upload

---
+++ q-no-41.js
@@ -0,0 +1,9 @@
+"use strict";
+// Diffine a function each magician name from an array
+function show_magicians(magicians) {
+    magicians.forEach(name => console.log(name));
+}
+// Define an array containing magicians name
+let magician_name = ["Harry Black Stone", "Siegfried", "David Blaine"];
+// Call the  function to print each magician name
+show_magicians(magician_name);
---
+++ q-no-41.ts
@@ -0,0 +1,13 @@
+// Diffine a function each magician name from an array
+function show_magicians(magicians: string[]){
+    magicians.forEach (name =>console.log(name)); 
+}
+
+// Define an array containing magicians name
+
+let magician_name=["Harry Black Stone","Siegfried","David Blaine"]
+
+ // Call the  function to print each magician name
+
+ show_magicians(magician_name);
+ 
