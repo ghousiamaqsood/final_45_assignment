5a483eea1b1ec960bc32b9c9226e26596cbc55a2
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:11:14 +0500

    Add files via upload

---
+++ q-no-05.js
@@ -0,0 +1,4 @@
+"use strict";
+let famousPerson = "Albert Einstein";
+let message = "Once said, A person who never made a mistke never tried anything new";
+console.log("famousperson, message");
---
+++ q-no-05.ts
@@ -0,0 +1,7 @@
+
+let famousPerson = "Albert Einstein";
+
+let message = "Once said, A person who never made a mistke never tried anything new";
+console.log("famousperson, message");
+
+
