422e36e5539f58a6a35ccb453db0d52971a5afff
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:35:29 +0500

    Add files via upload

---
+++ q-no-13.js
@@ -0,0 +1,5 @@
+"use strict";
+let trnsportation = ["motercycle", "car", "van", "jeep", "rail",];
+trnsportation.forEach(name => {
+    console.log(`"I would like to own a ${name}"`);
+});
---
+++ q-no-13.ts
@@ -0,0 +1,4 @@
+let trnsportation:string[] = ["motercycle","car","van","jeep","rail",];
+trnsportation.forEach(name =>{
+    console .log(`"I would like to own a ${name}"`)
+}) 
