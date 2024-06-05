b99f5d309b5f453823f5c78e350914a33b0961b1
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:28:54 +0500

    Add files via upload

---
+++ q-no-10.js
@@ -0,0 +1,12 @@
+"use strict";
+// Additio 
+console.log(4 + 4);
+// Subtraction
+console.log(12 - 4);
+// Multilication
+console.log(4 * 2);
+// Division
+console.log(16 / 2);
+// My name i masqsood i am enrollin governor sindh initiative progerame
+let My_message, message = ("my name i masqsood {i am enrollin governor sindh initiative progerame}batch first");
+console.log(My_message);
