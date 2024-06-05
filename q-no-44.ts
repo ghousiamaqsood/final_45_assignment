2a392e5eb18233f2a89ae48314ae6ef90f73b4b1
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 14:10:08 +0500

    Add files via upload

---
+++ q-no-44.ts
@@ -0,0 +1,14 @@
+// Define a function with a rest parameter that accept items arugment representing our sadwiches
+function makeSadwiches(...items: string[]){
+    console.log("\nMaking a sandwich with following items: \n");
+    items. forEach(Singleitem=>console.log(Singleitem));
+    console.log("\nNow Enjoy Sandwich");
+    
+
+
+}
+
+// Call the function 3 times with 3 different number of aeguments
+ makeSadwiches("Chicken","Cheese","Mayo",);
+ makeSadwiches("Bread","chicken","vegetable","butter","cheese");
+makeSadwiches("Chicken","Cheese","Mayonnaise","kechup");
