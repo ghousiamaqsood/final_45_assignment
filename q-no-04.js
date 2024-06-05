7010ffdb98201a2894ab62f86ec2826d375b1250
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-05 13:08:58 +0500

    Add files via upload

---
+++ q-no-04.js
@@ -0,0 +1,3 @@
+"use strict";
+let famous_person = "Allama muhammad iqbal once said,people who have no hold over";
+console.log(famous_person);
---
+++ q-no-04.ts
@@ -0,0 +1,2 @@
+let famous_person:string = "Allama muhammad iqbal once said,people who have no hold over";
+console.log(famous_person);
