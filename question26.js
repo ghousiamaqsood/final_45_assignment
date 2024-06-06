fef91e2d54f4dbfd195c108ff3903866a0cab829
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-06 12:01:34 +0500

    Add files via upload

---
+++ question26.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_Color = "green";
+if (alien_Color === "green") {
+    console.log("\nthe playerjust earned 5 points");
+}
+else {
+    console.log("the playerjust earned 10 points.");
+}
+alien_Color = "yellow";
+if (alien_Color === "green") {
+    console.log("the playerjust earned 5 points.");
+}
+else {
+    console.log("the playerjust earned 10 points.");
+}
---
+++ question26.ts
@@ -0,0 +1,22 @@
+let alien_Color = "green";
+if (alien_Color ==="green"){
+
+
+console.log("\nthe playerjust earned 5 points");
+}
+
+    
+ else{
+    console.log("the playerjust earned 10 points.");
+}
+
+ alien_Color = "yellow";
+ if (alien_Color ==="green"){
+
+    console.log("the playerjust earned 5 points.");
+ }
+
+ else{
+    console.log("the playerjust earned 10 points.");
+ }
+
