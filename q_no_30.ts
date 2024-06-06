90f5862a918a0b700a6993ccac5fbf432454dfe9
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-06 22:07:58 +0500

    Add files via upload

---
+++ q_no_30.ts
@@ -0,0 +1,26 @@
+  let user_Names: string []=["Admin","Fouzia","Ghousia","Anabia","Alisha"]
+
+  user_Names.forEach(userName =>{
+
+  if(userName === "Admin"){
+   
+    console.log(`Hello",${userName},Would you like to see a status report`); 
+  }
+else
+{
+}
+  console.log(`\nHello",${userName},Thank you loggin in again`);
+
+  }
+  
+  
+)
+{
+
+}
+
+
+    
+
+    
+
