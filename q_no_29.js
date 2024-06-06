7e6d4e81758a197af31cd31265f8a3fa8337938c
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-06 16:16:58 +0500

    Add files via upload

---
+++ q_no_29.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let favotiret_fruit = ["Mango", "Apple", "Orange",];
+if (favotiret_fruit.includes("Mango")) {
+    console.log("you really like Mango!");
+}
+if (favotiret_fruit.includes("Apple")) {
+    console.log("you really like Apple!");
+}
+if (favotiret_fruit.includes("Orange")) {
+    console.log("you really like Orange!");
+}
+if (favotiret_fruit.includes("Stawberry")) {
+    console.log("you really like stawbrrey");
+}
+if (favotiret_fruit.includes("Banana")) {
+    console.log("you really like Banana");
+}
