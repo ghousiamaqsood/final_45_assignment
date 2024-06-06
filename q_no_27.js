f13222731ac1c2a3d6710361dbbae9769c27a2f9
ghousiamaqsood <164852766+ghousiamaqsood@users.noreply.github.com>
2024-06-06 14:26:52 +0500

    Add files via upload

---
+++ q_no_27.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_color = "green";
+console.log("the plyer just earned 5 point");
+if (alien_color === "green") {
+    console.log("the plyer just earned 5 point");
+}
+else if (alien_color === "yellow") {
+    console.log("the plyer just earned 10 point");
+}
+else if (alien_color === "red") {
+    console.log("the plyer just earned 15 point");
+}
+else {
+    console.log("unknown color");
+}
+alien_color = "yellow";
+console.log("the plyer just earned 5 point");
+if (alien_color === "green") {
+    console.log("the plyer just earned 5 point");
+}
+else if (alien_color === "yellow") {
+    console.log("the plyer just earned 10 point");
+}
+else if (alien_color === "red") {
+    console.log("the plyer just earned 15 point");
+}
+else {
+    console.log("unknown color");
+}
+alien_color = "red";
+console.log("the plyer just earned 5 point");
+if (alien_color === "green") {
+    console.log("the plyer just earned 5 point");
+}
+else if (alien_color === "yellow") {
+    console.log("the plyer just earned 10 point");
+}
+else if (alien_color === "red") {
+    console.log("the plyer just earned 15 point");
+}
+else {
+    console.log("unknown color");
+}
---
+++ q_no_27.ts
@@ -0,0 +1,73 @@
+let alien_color = "green";
+console.log("the plyer just earned 5 point");
+
+if ( alien_color === "green"){
+    
+    console.log("the plyer just earned 5 point");
+}
+
+    
+ else if (alien_color === "yellow"){
+ console.log("the plyer just earned 10 point"); 
+
+ }
+
+    
+  else if (alien_color === "red"){
+    console.log("the plyer just earned 15 point"); 
+    
+     }else{
+
+       console.log("unknown color");
+
+
+     }
+      alien_color = "yellow";
+console.log("the plyer just earned 5 point");
+
+if ( alien_color === "green"){
+    
+    console.log("the plyer just earned 5 point");
+}
+
+    
+ else if (alien_color === "yellow"){
+ console.log("the plyer just earned 10 point"); 
+
+ }
+
+    
+  else if (alien_color === "red"){
+    console.log("the plyer just earned 15 point"); 
+    
+     }else{
+
+       console.log("unknown color");
+       
+
+     }
+
+      alien_color = "red";
+console.log("the plyer just earned 5 point");
+
+if ( alien_color === "green"){
+    
+    console.log("the plyer just earned 5 point");
+}
+
+    
+ else if (alien_color === "yellow"){
+ console.log("the plyer just earned 10 point"); 
+
+ }
+
+    
+  else if (alien_color === "red"){
+    console.log("the plyer just earned 15 point"); 
+    
+     }else{
+
+       console.log("unknown color");
+       
+
+     }
